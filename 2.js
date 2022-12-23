11c8523e4875a6baac9100e6be2be46f3b30e98b
dazfaz77 <dazfazm@gmail.com>
2022-12-23 07:51:56 +0100

    git push

--- 2.js
+++ 2.js
@@ -6,8 +6,8 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const katakana = 'loVEEE';
-const latin = '';
-const nums = 'LOoooooooooove';
+const latin = 'xxxaaaxxx';
+const nums = '000011100001100110100001100101001000001';
 
 const alphabet = katakana + latin + nums;
 
@@ -21,23 +21,22 @@ for( let x = 0; x < columns; x++ ) {
 }
 
 
-const draw = () => {
+function draw() {
     context.fillStyle = 'rgba(0, 0, 0, 0.05)';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     context.fillStyle = '#1F0';
     context.font = fontSize + 'px monospace';
 
-    for(let i = 0; i < rainDrops.length; i++)
-    {
+    for (let i = 0; i < rainDrops.length; i++) {
         const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-        context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+        context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
 
-        if(rainDrops[i]*fontSize > canvas.height && Math.random() > .96){
+        if (rainDrops[i] * fontSize > canvas.height && Math.random() > .96) {
             rainDrops[i] = 0;
         }
         rainDrops[i]++;
     }
-};
+}
 
 setInterval(draw, 45);
